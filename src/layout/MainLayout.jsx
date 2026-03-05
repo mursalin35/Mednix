074e6a5199f79e482997a95963ff4063e1d4ecdc
mursalin35 <msmursalin35@gmail.com>
2026-03-05 11:30:58 +0600

    feat: setup initial project with core folders

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from '../pages/HomePage';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <HomePage/>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/routes/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import HomePage from "../pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+export default router;
